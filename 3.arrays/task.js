96c835c9c7dba09245d9b22953c86957844228ec
Sam RUS <baxteevs@mail.ru>
2024-11-09 23:15:07 +0700

    Задание сделано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
-}
 
-function getUsersNamesInAgeRange(users, gender) {
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); 
+  }
   
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    
+    let mas = users.filter(item => item.gender === gender).map(item => item.age);
+    let res = mas.reduce((sum, age) => sum + age, 0);
+    
+    return (mas.length > 0) ? res / mas.length : 0;
+    
+    }
