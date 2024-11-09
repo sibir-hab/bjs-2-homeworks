0d6094d34c4fad9287759cbd45edf9b83f568563
Sam RUS <baxteevs@mail.ru>
2024-11-09 22:57:17 +0700

    Задание сделано

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
+//Функция-конструктор.//
 function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
+  //Предмет экземпляра.//
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
+  //Проверка оценок на существование и их отправка.// 
+  Student.prototype.addMarks = function (...marks) {
+    if (this.hasOwnProperty(`marks`) === true) {
+      this.marks.push(...marks);
+    }
+  };
+  //Проверка наличия оценок и возвращение среднего арефмитического.//
+  Student.prototype.getAverage = function () {
+    if (this.hasOwnProperty(`marks`) === false || this.marks.length === 0) {
+      return 0;
+    }
+    const ballAverage = this.marks.reduce((acc, mark, index, arr) => {
+      acc += mark;
+      if (index === arr.length - 1) {
+        return acc / arr.length;
+      }
+      return acc;
+    }, 0);
+    return ballAverage;
+  };
+  //Исключать студента и находить причину.//
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
